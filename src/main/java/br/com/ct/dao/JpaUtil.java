80ef384c600b44a0e5c002e410add571c281ae5b
Lucier F Lima <lucierflima@gmail.com>
2019-05-22 01:25:17 -0300

    alterar banco

--- src/main/java/br/com/ct/dao/JpaUtil.java
+++ src/main/java/br/com/ct/dao/JpaUtil.java
@@ -1,11 +1,5 @@
 package br.com.ct.dao;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,15 +10,5 @@ public class JpaUtil {
 	public static EntityManager getEntityManager() {
 		return emf.createEntityManager();
 	}
-	
-	@SuppressWarnings("unused")
-	private static Connection getConnection() throws URISyntaxException, SQLException {
-	    URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 
-	    String username = dbUri.getUserInfo().split(":")[0];
-	    String password = dbUri.getUserInfo().split(":")[1];
-	    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-
-	    return DriverManager.getConnection(dbUrl, username, password);
-	}
 }
